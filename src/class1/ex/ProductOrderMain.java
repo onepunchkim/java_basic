b24d876c3271c31a3bbb5e9866d58758fbba569a
성스러운 성진이 <hyukjin4711@gmail.com>
2024-08-20 21:11:29 +0900

    ref study

--- src/class1/ex/ProductOrderMain.java
+++ src/class1/ex/ProductOrderMain.java
@@ -4,23 +4,23 @@ public class ProductOrderMain {
     public static void main(String[] args) {
         ProductOrder[] orders = new ProductOrder[3];
 
-        ProductOrder orderr1 = new ProductOrder();
-        orderr1.productName = "두부";
-        orderr1.price = 2000;
-        orderr1.quantity = 2;
-        orders[0] = orderr1;
+        ProductOrder order1 = new ProductOrder();
+        order1.productName = "두부";
+        order1.price = 2000;
+        order1.quantity = 2;
+        orders[0] = order1;
 
-        ProductOrder orderr2 = new ProductOrder();
-        orderr2.productName = "김치";
-        orderr2.price = 5000;
-        orderr2.quantity = 1;
-        orders[1] = orderr2;
+        ProductOrder order2 = new ProductOrder();
+        order2.productName = "김치";
+        order2.price = 5000;
+        order2.quantity = 1;
+        orders[1] = order2;
 
-        ProductOrder orderr3 = new ProductOrder();
-        orderr3.productName = "콜라";
-        orderr3.price = 1500;
-        orderr3.quantity = 2;
-        orders[2] = orderr3;
+        ProductOrder order3 = new ProductOrder();
+        order3.productName = "콜라";
+        order3.price = 1500;
+        order3.quantity = 2;
+        orders[2] = order3;
 
         int totalAmount = 0;
         for (ProductOrder order : orders) {
