ca1061db2b1f3ad1f4638d519dd1fda73832e6ac
onepunchkim <hyukjin4711@gmail.com>
2024-08-30 19:00:50 +0900

    static study

--- src/static2/DecoDataMain.java
+++ src/static2/DecoDataMain.java
@@ -1,5 +1,8 @@
 package static2;
 
+//import static static2.DecoData.staticCall;
+//import static static2.DecoData.*;
+
 public class DecoDataMain {
     public static void main(String[] args) {
         System.out.println("1.정적 호출");
@@ -12,5 +15,13 @@ public class DecoDataMain {
         System.out.println("3.인스턴스 호출2");
         DecoData data2 = new DecoData();
         data2.instanceCall();
+
+        //추가
+        //인스터스를 통한 접근
+        DecoData data3 = new DecoData();
+        DecoData.staticCall(); //자바가 추천하지 않음 -> 코드를 읽을 때, 인스턴스 메서드에 접근하는 것처럼 오해하기 때문
+
+        //클래스를 통한 접근
+        DecoData.staticCall();
     }
 }
