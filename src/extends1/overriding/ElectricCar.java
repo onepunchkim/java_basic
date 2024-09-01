85f32c2f822bfbb46a4076f9a6a5297b6988b3b2
onepunchkim <hyukjin4711@gmail.com>
2024-09-01 18:47:45 +0900

    extends study

--- src/extends1/overriding/ElectricCar.java
+++ src/extends1/overriding/ElectricCar.java
@@ -3,6 +3,7 @@ package extends1.overriding;
 public class ElectricCar extends Car {
 
     @Override //부모의 기능을 자식이 새로 재정의하는 것을 메서드 오버라이딩이라 한다.
+    //실수로 오버라이딩을 못하는 경우를 방지해준다.
     public void move(){
         System.out.println("전기차를 빠르게 이동합니다.");
     }
