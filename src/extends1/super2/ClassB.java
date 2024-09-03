3f0aa3401f34190823a06d4810bd466f0a003d44
onepunchkim <hyukjin4711@gmail.com>
2024-09-03 15:16:34 +0900

    extends study

--- src/extends1/super2/ClassB.java
+++ src/extends1/super2/ClassB.java
@@ -2,7 +2,7 @@ package extends1.super2;
 
 public class ClassB extends ClassA{
     public ClassB(int a){
-        super(); //기본 생성자 생략 가능
+        this(a, 0); //기본 생성자 생략 가능
         System.out.println("ClassB 생성자 a=" + a);
     }
 
