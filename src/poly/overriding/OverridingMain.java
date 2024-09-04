4fe86d55e2ecff6f9daec0e1e31565b8d11a3171
성스러운 성진이 <hyukjin4711@gmail.com>
2024-09-04 20:44:13 +0900

    poly study

--- src/poly/overriding/OverridingMain.java
+++ src/poly/overriding/OverridingMain.java
@@ -13,5 +13,11 @@ public class OverridingMain {
         System.out.println("Parent -> Parent");
         System.out.println("value = " + parent.value);
         parent.method();
+
+        //부모 변수가 자식 인스턴스 참조(다형적 참조)
+        Parent poly = new Child();
+        System.out.println("Parent -> Child");
+        System.out.println("value = " + poly.value); //변수는 오버라이딩X
+        poly.method(); //메서드 오버라이딩!
     }
 }
